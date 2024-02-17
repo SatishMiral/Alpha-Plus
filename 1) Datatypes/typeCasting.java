5b502b9d237a0824e1f60a5126a3ec7c06144e27
SatishMiral <144206894+SatishMiral@users.noreply.github.com>
2024-02-17 11:41:57 +0530

    Add files via upload

---
+++ 1) Datatypes/typeCasting.java
@@ -0,0 +1,14 @@
+public class typeCasting {
+    public static void main(String[] args) {
+        float a = 25.12f;
+        int b = (int) a;
+        System.out.println(b);
+
+        char ch = 'a';
+        char ch2 = 'b';
+        int num = ch;
+        int num2 = ch2;
+        System.out.println(num);
+        System.out.println(num2);
+    }
+}
---
+++ 1) Datatypes/variables.java
@@ -0,0 +1,10 @@
+public class variables {
+    public static void main(String[] args) {
+        int a =10;
+        double b = 10.10;
+        String name = "Sat";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(name);
+    }
+}
